ef812665d7c43d8a9008c270e458b68a41437239
ShriBanuPriya <ShriBanuPriya@users.noreply.github.com>
2018-11-06 15:47:44 +0530

    carouse, effects added

---
+++ js/script.js
@@ -0,0 +1,49 @@
+
+$(document).ready(function(){
+    $(".click1").click(function(){
+        $(".panel1").slideToggle("slow");
+    });
+    $(".click2").click(function(){
+        $(".panel2").slideToggle("slow");
+    });
+    $(".click3").click(function(){
+        $(".panel3").slideToggle("slow");
+    });
+    $(".click4").click(function(){
+        $(".panel4").slideToggle("slow");
+    });
+    $(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+        $('[data-toggle="popover"]').popover();
+
+
+
+
+
+            $("form[name='registration']").validate({
+          rules: {
+            name: {
+              required: true
+
+            },
+            email: {
+              required: true,
+              email: true
+            },
+            message: {
+              required: true
+            }
+          },
+          messages: {
+            name: "Please specify your name",
+            email: "Please specify a valid email address",
+           message: "Please specify your message"
+          },
+          submitHandler: function() {
+            alert("Form Submitted successfully");
+          }
+        });
+      
+
+});
